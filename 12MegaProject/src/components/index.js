6e9097c217e609138c6aba29b46438fcd168048a
abhimanyujangid <abhimanyujangid79@gmail.com>
2024-09-21 01:59:13 +0530

    change and added logo,button , logout

--- 12MegaProject/src/components/index.js
+++ 12MegaProject/src/components/index.js
@@ -2,8 +2,12 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo/Logo";
 import Button from "./Button/Button";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
-    Footer  
+    Footer ,
+    Logo,
+    Button,
+    LogoutBtn
 };
