fa1d59bfb0228767d3504a05c668819b53fb16ca
abhimanyujangid <abhimanyujangid79@gmail.com>
2024-09-21 22:33:51 +0530

    Add select.jsx

--- 12MegaProject/src/components/index.js
+++ 12MegaProject/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/Container";
 
 export {
     Header,
     Footer ,
     Logo,
     Button,
-    LogoutBtn
+    LogoutBtn,
+    Container
 };
