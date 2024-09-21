adcc7fd220b1e73007b4748ce388f5c5d3a08602
abhimanyujangid <abhimanyujangid79@gmail.com>
2024-09-21 23:51:40 +0530

    Add authLayout

---
+++ 12MegaProject/src/components/AuthLayout.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children, authentication = true}) {
+    
+    const navigate = useNavigate()
+    const  [loader,setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(()=>{
+        // TODO: Make it more easy
+        // if(authStatus === false){
+        //     navigate('/login')
+        // }else if(authStatus === true){
+        //     navigate('/')
+        // }
+
+        if(authentication && authStatus !== authentication){
+            navigate('/login')
+        }
+        else if(!authentication && authStatus !== authentication){
+            navigate('/')
+        }
+        setLoader(false)
+    },[authStatus,authentication,navigate])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+  // return children    
+}
+
